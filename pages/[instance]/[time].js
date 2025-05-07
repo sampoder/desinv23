026d311ab3dcb60be071be81ff679bf9bfda0aad
Sam Poder <39828164+sampoder@users.noreply.github.com>
2025-05-06 17:24:22 -0700

    do it in react

--- pages/[instance]/[time].js
+++ pages/[instance]/[time].js
@@ -3,54 +3,31 @@
 import { useRouter } from "next/router";
 import { list } from "@vercel/blob";
 import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
-const ReactP5Wrapper = dynamic(
-  () => import("@p5-wrapper/react").then((mod) => mod.ReactP5Wrapper),
-  {
-    ssr: false,
-  },
-);
-
-function sketch(p5) {
-  let images = [];
-  let loaded = [];
-
-  p5.updateWithProps = (props) => {
-    if (props.images) {
-      images = [...props.images.map((image, index) => p5.loadImage(image))];
-      console.log(props.images)
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+ 
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+ 
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
     }
-  };
-
-  p5.setup = () => {
-    p5.createCanvas(p5.windowWidth, p5.windowHeight, p5.WEBGL);
-    p5.frameRate(5);
-  };
-
-  p5.draw = () => {
-    if (images.length > 0) {
-      let img = images.length == 1 ? images[0] : images.shift()
-      if(p5.windowHeight > p5.windowWidth) {
-        img.resize(0, p5.windowHeight)
-      } else {
-        img.resize(p5.windowWidth, 0)
-      }
-      p5.imageMode(p5.CENTER)
-      p5.image(
-        img,
-        0,
-        0,
-      );
-      p5.noLoop();
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
     }
-  };
+  }, [delay]);
 }
 
 export default function Viewer({ blobs }) {
   const router = useRouter();
   const [images, setImages] = useState([]);
-  const [viewCount, setViewCount] = useState(1);
 
   const imageUrlToBase64 = async (url) => {
     const response = await fetch(url);
@@ -75,53 +52,53 @@ export default function Viewer({ blobs }) {
       },
     );
   }, []);
+  
+  let [count, setCount] = useState(0);
+  
+  useInterval(() => {
+    // Your custom logic here
+    setCount(count + 1);
+  }, 100);
 
   return (
-  
-      <div>
-        <ReactP5Wrapper
-          sketch={sketch}
-          images={images}
-          viewCount={viewCount}
-        />
-        {!(images.length > 0) && (
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              color: "white",
-              fontSize: 36,
-              width: '100vw',
-              height: '100vh',
-              position: 'absolute',
-              top: 0,
-              left: 0
-            }}
-          >
-            Loading...
-          </div>
-        )}
+    <div>
+      {!(images.length > 0) ? (
         <div
           style={{
-            position: "absolute",
-            bottom: 16,
-            left: 16,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "white",
+            fontSize: 36,
             width: '100vw',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            fontFamily: 'system-ui',
-            fontSize: 28
+            height: '100vh',
+            position: 'absolute',
+            top: 0,
+            left: 0
           }}
-          onClick={() => setViewCount(viewCount + 1)}
         >
-          <div style={{background: "black", padding: "16px 32px", borderRadius: 8, display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center', cursor: 'pointer'}}>🔁 <span style={{ display: 'inline-block', marginLeft: '12px', fontWeight: 800, transform: 'translateY(-0.5px)' }}>Replay</span></div>
+          Loading...
         </div>
+      ) : <img src={images[count] || images[images.length - 1]} style={{ objectFit: 'cover', height: '100vh', width: '100vw'}} />}
+      <div
+        style={{
+          position: "absolute",
+          bottom: 16,
+          left: 16,
+          width: '100vw',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontFamily: 'system-ui',
+          fontSize: 28
+        }}
+        onClick={() => setCount(0)}
+      >
+        <div style={{background: "black", padding: "16px 32px", borderRadius: 8, display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center', cursor: 'pointer'}}>🔁 <span style={{ display: 'inline-block', marginLeft: '12px', fontWeight: 800, transform: 'translateY(-0.5px)' }}>Replay</span></div>
       </div>
-  
+    </div>
   );
 }
 

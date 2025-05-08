bcb7d664dc1204fcf9adbd2c7a3efb03b2a03530
Sam Poder <sam.r.poder@gmail.com>
2025-05-07 22:20:46 -0500

    Update save.js

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,4 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// This API route is used to save an image into Vercel Blob. Instances are to differentiate between different times the art is displayed.
+// The fetching is done to convert the image into a format Vercel can accept.
 
 import { put } from '@vercel/blob';
 
@@ -11,6 +12,8 @@ export default async function handler(req, res) {
   
   return res.json(blob);
 }
+
+// this is so the route can accept large images
 
 export const config = {
     api: {

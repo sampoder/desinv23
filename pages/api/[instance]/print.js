ac946ee77d097228f4fc412b2d13a20a02d7d9f1
Sam Poder <sam.r.poder@gmail.com>
2025-05-07 22:30:52 -0500

    Update print.js

--- pages/api/[instance]/print.js
+++ pages/api/[instance]/print.js
@@ -2,10 +2,10 @@ import { list } from "@vercel/blob";
 import { getAll } from '@vercel/edge-config';
 
 export async function getBlob(instance){
-  let { blobs } = await list({ prefix: instance });
-  let configItems = await getAll();
+  let { blobs } = await list({ prefix: instance }); // get all images
+  let configItems = await getAll(); // get all the times someone has "captured"
   configItems = Object.entries(configItems)
-  configItems = configItems.sort((a, b) => parseInt(b[0]) - parseInt(a[0]))
+  configItems = configItems.sort((a, b) => parseInt(b[0]) - parseInt(a[0])) // get the most recent capture
   
   let displaying = configItems[0][0]
   console.log(displaying)
@@ -16,15 +16,15 @@ export async function getBlob(instance){
     .map((blob) => ({
       url: blob.url,
       timestamp: parseInt(blob.pathname.split("/")[1]),
-    }));
-  let blob = blobs[blobs.length - 1]
+    })); // select all photos that are at or before the time of capture
+  let blob = blobs[blobs.length - 1] // get the last blob to print
   return blob
 }
 
 export default async function handler(req, res) {
   let configItems = await getAll();
   configItems = Object.entries(configItems)
-  configItems = configItems.sort((a, b) => parseInt(b[0]) - parseInt(a[0]))
+  configItems = configItems.sort((a, b) => parseInt(b[0]) - parseInt(a[0])) // this is the same logic to get the most recent "capture" to generate the URL which is used by the Siri shortcut
   
   let displaying = configItems[0][0]
   return res.json({blob: await getBlob(req.query.instance), url: `https://photobooth.sampoder.com/${req.query.instance}/${displaying}`});
